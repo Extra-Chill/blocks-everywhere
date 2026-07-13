4ae9eea264cb1ed5774e8247db85963d1d37a1f7
Chris Huber <chubes@extrachill.com>
2026-07-13 12:46:01 -0400

    test: follow source contracts to owning modules (#118)

--- tests/test-public-lifecycle-events.php
+++ tests/test-public-lifecycle-events.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 // phpcs:ignore
 class Public_Lifecycle_Events_Test extends TestCase {
 	public function test_public_dom_lifecycle_events_use_sanitized_detail() {
-		$source = file_get_contents( dirname( __DIR__ ) . '/src/editor/index.tsx' );
+		$source = file_get_contents( dirname( __DIR__ ) . '/src/editor/lifecycle.ts' );
 
 		$this->assertStringContainsString( 'function createPublicLifecycleEventDetail', $source );
 		$this->assertStringContainsString( 'detail: createPublicLifecycleEventDetail( eventDetail )', $source );
